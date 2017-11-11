d1f42e820bcf89d8740560a8db7b0fbe4eb2abd9
Matthew Schofield <Matthewschofield71@yahoo.co.uk>
2017-11-11 20:25:57 +0000

    Added UI

---
+++ app/src/main/java/com/example/matth/calcam/SetTargetsActivity.java
@@ -0,0 +1,13 @@
+package com.example.matth.calcam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SetTargetsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_targets);
+    }
+}
