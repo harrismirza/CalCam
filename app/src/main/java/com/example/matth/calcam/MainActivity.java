1119880728e28eb6f73332fe1283cd5f0e4306fd
Emre <emre.kahraman@kcl.ac.uk>
2017-11-12 02:27:42 +0100

    Added activity

--- app/src/main/java/com/example/matth/calcam/MainActivity.java
+++ app/src/main/java/com/example/matth/calcam/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.matth.calcam;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openCamera(View view) {
+        Intent newActivity = new Intent(this, BarcodeCaptureActivity.class);
+        newActivity.putExtra(BarcodeCaptureActivity.AutoFocus, true);
+        newActivity.putExtra(BarcodeCaptureActivity.UseFlash, false);
+        startActivityForResult(newActivity, 9001);
     }
 }
